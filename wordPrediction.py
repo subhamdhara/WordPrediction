dff1ec8f18f7ac1d28dd5edbd8436636d9538eb1
Bhaskar Dhara <dharabhaskar@gmail.com>
2022-01-12 13:59:54 +0530

    initial commit

---
+++ wordPrediction.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+words = list({'rainbow', 'computer', 'science', 'programming',
+              'python', 'mathematics', 'player', 'condition',
+              'reverse', 'water', 'board', 'food', "bench", "chair", "table",
+              "spell", "java", "ruby", "rugby", "football", "oak"})
+
+r = random.choice(words)
+
+print("Guess the characters")
+for _ in range(len(r)):
+    print("-")
+    time.sleep(0.1)
+
+userW = []
+
+for i in range(len(r)):
+    user = input("Enter the character: ")
+    while user != r[i]:
+        if user == "hint":
+            print("starts with", r[0])
+        else:
+            print("try again")
+        user = input("Enter the character: ")
+
+    userW.append(user)
+
+    for i in range(len(r)):
+        try:
+            print(userW[i])
+        except IndexError:
+            print("_")
+else:
+    print("You got it correct, it's", r)
